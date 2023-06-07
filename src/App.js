51bd3ca9cd625cfa517cf5a84bd3118818880a4a
Michellefsd <promichfsd@gmail.com>
2023-06-07 18:00:15 -0300

    api rest call, intro

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const apiCall = async() => {
+    const api = await fetch('https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=czVLEVgvpCDfpkJKALhxswhQ5VoLxpu5');
+    console.log(api);
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +13,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className='App-link' onClick={apiCall}>NEWSSSSS</button>
+        
       </header>
     </div>
   );
---
+++ src/dist/index.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+var _client = require("react-dom/client");
+
+var _App = _interopRequireDefault(require("./App"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var el = document.getElementById('root');
+var root = (0, _client.createRoot)(el);
+root.render();
+//# sourceMappingURL=index.dev.js.map
