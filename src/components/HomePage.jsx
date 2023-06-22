575e566bf341745c9749cffdabf9d52fabb5fa85
Michellefsd <promichfsd@gmail.com>
2023-06-22 12:24:20 -0300

    prolijo,  q pasa con ese margin q no piude sacar del body?

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
 import { useState, useEffect } from 'react';
 import { getNASA, getApi } from '../functions/function';
+import { ImSpinner3 } from 'react-icons/im';
 
 function HomePage() {
     const [nasaNews, setNasaNews] = useState(null)
     useEffect(() => {
         getNASA(setNasaNews)
     }, [])
-    // const [api, setApi] = useState(null)
+    const style = {
+      "animation": "spin infinite 3.3s linear",
+      "color":"rgb(210, 93, 240)",
+      "fontSize":"4em"
+    }
+  return (
+    <div className='container'>
+        {nasaNews? (
+            <div className='content'>
+                <img src={nasaNews.url} alt={nasaNews.title}/>
+                <h3>{nasaNews.title}</h3>
+                <div>
+                    <p className='text'>{nasaNews.copyright} - {nasaNews.date}</p>
+                    <div className='text-div'>
+                      <p className='text'>{nasaNews.explanation}</p>
+                    </div>
+                </div>
+            </div>
+        ): <div className='title'><ImSpinner3 style={style}/></div>}
+        <footer className='footer'>
+            <p>This web page was build thanks to Nasa Api.</p>
+            <p>It shows the picture of the day and a brief explanation about it. Come back tomorrow to see another interesting info.</p>
+            <p>Michelle Rodriguez Developer </p>
+        </footer>
+    </div>
+  );
+}
+
+export default HomePage;
+
+
+        // const [api, setApi] = useState(null)
     // const [refresh, setRefresh] = useState(false)
     // const rerender = () => {
     //     setRefresh(!refresh)
@@ -15,31 +46,9 @@ function HomePage() {
     // useEffect(() => {
     //     getApi(setApi)
     // }, [setRefresh])
-  return (
-    <div className='container'>
-        {nasaNews? (
-            <div className='content'>
-                <img src={nasaNews.url} alt={nasaNews.title}/>
-                <h3>{nasaNews.title}</h3>
-                <div className='text'>
-                    <p>{nasaNews.copyright} - {nasaNews.date}</p>
-                    <p>{nasaNews.explanation}</p>
-                </div>
-            </div>
-        ): "is Loading"}
-        <footer className='footer'>
-            <p>This webPage was build thanks to Nasa Api.</p>
-            <p>It shows the picture of the day and a brief explanation about it.</p>
-            <p>Come back tomorrow to see another interesting info.</p>
-        </footer>
-        {/* {api? (
-            <div>
-                <p>{api}</p>
-                <button onClick={rerender}>refresh </button>
-            </div>
-        ): "is Loading"} */}
-    </div>
-  );
-}
-
-export default HomePage;
+    //    {/* {api? (
+      //      <div>
+        //        <p>{api}</p>
+          //      <button onClick={rerender}>refresh </button>
+            //</div>
+    //    ): "is Loading"} */}
