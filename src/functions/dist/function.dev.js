6b1da72de2b25c0546c3da73ce8554ccd44ae5bd
Michellefsd <promichfsd@gmail.com>
2023-06-22 16:02:02 -0300

    como hago deploy con la variable de entorno?

--- src/functions/dist/function.dev.js
+++ src/functions/dist/function.dev.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.getApi = exports.getNASA = void 0;
+exports.getNASA = void 0;
 
 var _axios = _interopRequireDefault(require("axios"));
 
@@ -15,44 +15,24 @@ var getNASA = function getNASA(state) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
-          console.log(process.env.REACT_APP_API_KEY);
-          _context.next = 3;
-          return regeneratorRuntime.awrap(_axios["default"].get("https://api.nasa.gov/planetary/apod?api_key=".concat(process.env.REACT_APP_API_KEY)));
+          _context.next = 2;
+          return regeneratorRuntime.awrap(_axios["default"].get("https://api.nasa.gov/planetary/apod?api_key=".concat(process.env.REACT_APP_API_KEY || "I8UhPoqXqnDK2im3ficSk47KNYIW7V7IgAG7NYcV")));
 
-        case 3:
+        case 2:
           request = _context.sent;
           state(request.data);
 
-        case 5:
+        case 4:
         case "end":
           return _context.stop();
       }
     }
   });
-};
+}; // export const getApi = async(state) => {
+//     const request = await axios.get("https://api.adviceslip.com/advice")
+//     state(request.data.slip.advice)
+// }
+
 
 exports.getNASA = getNASA;
-
-var getApi = function getApi(state) {
-  var request;
-  return regeneratorRuntime.async(function getApi$(_context2) {
-    while (1) {
-      switch (_context2.prev = _context2.next) {
-        case 0:
-          _context2.next = 2;
-          return regeneratorRuntime.awrap(_axios["default"].get("https://api.adviceslip.com/advice"));
-
-        case 2:
-          request = _context2.sent;
-          state(request.data.slip.advice);
-
-        case 4:
-        case "end":
-          return _context2.stop();
-      }
-    }
-  });
-};
-
-exports.getApi = getApi;
 //# sourceMappingURL=function.dev.js.map
--- src/functions/function.js
+++ src/functions/function.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getNASA = async(state) => {
-    const request = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}`)
+    const request = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY || "I8UhPoqXqnDK2im3ficSk47KNYIW7V7IgAG7NYcV"}`)
     state(request.data)
 }
 // export const getApi = async(state) => {
